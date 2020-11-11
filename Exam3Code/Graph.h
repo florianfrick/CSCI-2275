6fbc4c301759183f3b3002367e1909c707898b03
Florian Frick <floriangfrick@hotmail.com>
2020-11-11 16:24:38 -0700

    Test3 Starter Files

---
+++ Exam3Code/Graph.h
@@ -0,0 +1,38 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<vector>
+#include<iostream>
+
+struct vertex;
+
+struct adjVertex{
+    vertex *v;
+    int weight;
+};
+
+struct vertex{
+    std::string name;
+    std::vector<adjVertex> adj;
+    bool visited = false;
+    int distance = 0;
+    vertex *parent = nullptr;
+};
+
+class Graph
+{
+    public:
+        Graph();
+        ~Graph();
+        void addEdge(std::string v1, std::string v2, int weight);
+        void addVertex(std::string name);
+        void displayEdges();
+    protected:
+    private:
+        //vector<edge> edges;
+        std::vector<vertex> vertices;
+        vertex* search(std::string v);
+        vertex* BFS(std::string v1, std::string v2);
+
+};
+
+#endif // GRAPH_H
